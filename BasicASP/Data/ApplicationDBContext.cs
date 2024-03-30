15f6a84dbeaf3c2740fb649d09965d08c67eace0
kanaji <kzcofx@gmail.com>
2024-03-30 21:40:10 +0700

    Add project files.

---
+++ BasicASP/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BasicASP.Models;
+
+namespace BasicASP.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { 
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ BasicASP/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicASP.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Full name")]
+        public string Name { get; set; }
+        [DisplayName("Test score")]
+        [Range(0,100)]
+        public int Score { get; set; }
+    }
+}
